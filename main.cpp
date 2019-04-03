6e5e63814cf41ff09e93d24c5bea9ed272e6baa2
SILVANO <silvanonsanches@gmail.com>
2019-04-02 23:42:33 -0300

    Mudanças no diagrama e no main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 using std::cout;
 int main(int argc, char **argv)
 {   
-    Linux tempo;
-    tempo.uptime();
-	<<"hello world\n";
+    Linux tempoDeAtividade("Tempo de atividade do sistema");
+    
+    cout<< " Contador do tempo de atividade do sistema " << tempoDeAtividade.getHoras()
+    <<end1;
+	//<<"hello world\n";
 	return 0;
 }
